6acd11bcda3cc0fe468cc7f69d0f370b85a78ffb
Camilo <milocosta@yahoo.com.br>
2021-04-12 18:19:03 -0300

    Criação do Wordcloud dos problemas

--- AnaliseBaseReclamacoes.py
+++ AnaliseBaseReclamacoes.py
@@ -53,11 +53,24 @@ print(df_estado.groupby('estado').size().sort_values(ascending=False))
 # %% Criação de uma string única com os estados
 s_estados = " ".join([text for text in df_estado['estado']])
 # %% Criação do Wordcloud dos Estados
-wordcloud_estados = WordCloud(background_color='black', collocations=False, colormap='GnBu', width=3000, height=2000,max_font_size=1000, max_words=40).generate(s_estados)
+wordcloud_estados = WordCloud(background_color='black', collocations=False, colormap='GnBu', width=3000, height=2000,max_font_size=1000, max_words=17).generate(s_estados)
 plt.figure(figsize=(40,30), facecolor='k', edgecolor='k')
 plt.imshow(wordcloud_estados, interpolation='bilinear')
 plt.axis('off')
 plt.tight_layout(pad=0)
 plt.savefig('WordcloudEstados.png')
 plt.show()
-# %%
+# %% # %% Para funcionamento do Wordcloud dos Problemas
+df_problema['ProblemaSemEspaco'] = df_problema.replace({'problema': dicionario}, regex=True)
+# %% Agrupando e Ordenando os problemas pela quantidade
+print(df_problema.groupby('problema').size().sort_values(ascending=False))
+# %% Criação de uma string única com os problemas
+s_problemas = " ".join([text for text in df_problema['ProblemaSemEspaco']])
+# %% Criação do Wordcloud dos Problemas
+wordcloud_problemas = WordCloud(background_color='black', collocations=False, colormap='GnBu', width=3000, height=2000,max_font_size=1000, max_words=40).generate(s_problemas)
+plt.figure(figsize=(40,30), facecolor='k', edgecolor='k')
+plt.imshow(wordcloud_problemas, interpolation='bilinear')
+plt.axis('off')
+plt.tight_layout(pad=0)
+plt.savefig('WordcloudProblemas.png')
+plt.show()
